dde459dd1dd1f38553908bbff0e6a4b2d0af287c
Ben Russell <benalvinrussell@gmail.com>
2021-10-10 22:28:02 -0400

    e

--- MaterialsApp/AboutFOrm.Designer.cs
+++ MaterialsApp/AboutFOrm.Designer.cs
@@ -36,42 +36,42 @@ namespace MaterialsApp
             // 
             // aboutHeaderLabel
             // 
-            this.aboutHeaderLabel.AutoSize = true;
+            this.aboutHeaderLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.aboutHeaderLabel.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.aboutHeaderLabel.Location = new System.Drawing.Point(27, 9);
+            this.aboutHeaderLabel.Location = new System.Drawing.Point(12, 9);
             this.aboutHeaderLabel.Name = "aboutHeaderLabel";
-            this.aboutHeaderLabel.Size = new System.Drawing.Size(197, 20);
+            this.aboutHeaderLabel.Size = new System.Drawing.Size(259, 20);
             this.aboutHeaderLabel.TabIndex = 0;
             this.aboutHeaderLabel.Text = "Materials List Estimator";
+            this.aboutHeaderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // aboutBodyLabel
             // 
-            this.aboutBodyLabel.AutoSize = true;
             this.aboutBodyLabel.Location = new System.Drawing.Point(12, 38);
             this.aboutBodyLabel.Name = "aboutBodyLabel";
-            this.aboutBodyLabel.Size = new System.Drawing.Size(227, 60);
+            this.aboutBodyLabel.Size = new System.Drawing.Size(259, 60);
             this.aboutBodyLabel.TabIndex = 1;
             this.aboutBodyLabel.Text = "A copy of the user manual was included\r\nin the .zip the application\'s executable " +
     "file\r\nwas located when downloaded. An online\r\ncopy can be found at the link belo" +
     "w.";
-            this.aboutBodyLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.aboutBodyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // aboutLinkLabel
             // 
-            this.aboutLinkLabel.AutoSize = true;
-            this.aboutLinkLabel.Location = new System.Drawing.Point(86, 111);
+            this.aboutLinkLabel.Location = new System.Drawing.Point(12, 107);
             this.aboutLinkLabel.Name = "aboutLinkLabel";
-            this.aboutLinkLabel.Size = new System.Drawing.Size(73, 15);
+            this.aboutLinkLabel.Size = new System.Drawing.Size(259, 21);
             this.aboutLinkLabel.TabIndex = 2;
             this.aboutLinkLabel.TabStop = true;
             this.aboutLinkLabel.Text = "User Manual";
+            this.aboutLinkLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.aboutLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.AboutLinkLabel_LinkClicked);
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 137);
+            this.ClientSize = new System.Drawing.Size(283, 137);
             this.Controls.Add(this.aboutLinkLabel);
             this.Controls.Add(this.aboutBodyLabel);
             this.Controls.Add(this.aboutHeaderLabel);
@@ -83,7 +83,6 @@ namespace MaterialsApp
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Documentation";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
